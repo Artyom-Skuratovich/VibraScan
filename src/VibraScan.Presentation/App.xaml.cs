0a95a91878e615e4b3151f5b457cfe507aacabb3
Artyom Skuratovich <artyomskuratovich@gmail.com>
2026-04-08 09:40:23 +0300

    добавлен слой Presentation на Wpf

---
+++ src/VibraScan.Presentation/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace VibraScan.Presentation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+
+    }
+}
