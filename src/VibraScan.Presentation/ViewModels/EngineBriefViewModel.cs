73d591d6fdc182e564afbf3b98aa748cec539ba3
Artyom Skuratovich <artyomskuratovich@gmail.com>
2026-05-14 17:17:24 +0300

    изменил стили, убрал лишнее

---
+++ src/VibraScan.Presentation/ViewModels/EngineBriefViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using VibraScan.Application.Engines.Queries.GetEngines;
+
+namespace VibraScan.Presentation.ViewModels
+{
+    public partial class EngineBriefViewModel : ObservableObject
+    {
+        [ObservableProperty] private EngineBriefDto _engine = null!;
+        [ObservableProperty] private bool _isChecked;
+        [ObservableProperty] private bool _canCheck;
+        [ObservableProperty] private bool _isVisible;
+
+        partial void OnCanCheckChanged(bool value)
+        {
+            IsVisible = value;
+            IsChecked = false;
+        }
+
+        [RelayCommand(CanExecute = nameof(CanCheck))]
+        private void Select()
+        {
+            IsChecked = !IsChecked;
+        }
+    }
+}
