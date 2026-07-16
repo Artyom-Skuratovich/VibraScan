226dd9a3506b10b09611ec14a147a40d99020656
Artyom Skuratovich <artyomskuratovich@gmail.com>
2026-07-16 08:13:10 +0300

    небольшие правки кода

--- src/VibraScan.Presentation/App.xaml.cs
+++ src/VibraScan.Presentation/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using LiveChartsCore;
+using LiveChartsCore.Kernel;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.IO;
 using System.Windows;
@@ -45,6 +47,8 @@ namespace VibraScan.Presentation
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+
+            LiveCharts.Configure(config => config.HasMap<float>((val, index) => new Coordinate(index, val)));
 
             var windowService = _serviceProvider.GetRequiredService<IWindowService>();
             windowService.Show<MigrationViewModel>();
