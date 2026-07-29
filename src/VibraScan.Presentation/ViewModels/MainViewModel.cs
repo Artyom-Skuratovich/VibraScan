1b7834d1a7429ba7440b389049dcdfe1326551d5
Artyom Skuratovich <artyomskuratovich@gmail.com>
2026-07-29 13:53:13 +0300

    feat: добавить шаблон окна с информацией о конкретном двигателе

--- src/VibraScan.Presentation/ViewModels/MainViewModel.cs
+++ src/VibraScan.Presentation/ViewModels/MainViewModel.cs
@@ -1,11 +1,19 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using VibraScan.Presentation.Common.Interfaces;
 
 namespace VibraScan.Presentation.ViewModels
 {
-    public partial class MainViewModel(ChartsViewModel chartsVm, MonitoringViewModel monitoringVm, DataImportViewModel dataImportVm) : ObservableObject
+    public partial class MainViewModel(ChartsViewModel chartsVm, MonitoringViewModel monitoringVm, DataImportViewModel dataImportVm) : ObservableObject, ICancellable
     {
         [ObservableProperty] private ChartsViewModel _chartsVm = chartsVm;
         [ObservableProperty] private MonitoringViewModel _monitoringVm = monitoringVm;
         [ObservableProperty] private DataImportViewModel _dataImportVm = dataImportVm;
+
+        public void Cancel()
+        {
+            ChartsVm.Cancel();
+            MonitoringVm.Cancel();
+            DataImportVm.Cancel();
+        }
     }
 }
